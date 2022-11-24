0a847e178e42de685fad23d931200b5cbe6dac0d
Berna <btnaberna@Bernas-Macbook-Air.local>
2022-11-24 15:58:55 +0000

    initial commit

---
+++ parentheses_checker_start/src/main/java/ParenthesesChecker.java
@@ -0,0 +1,19 @@
+public class ParenthesesChecker {
+
+    /* the goal is to write a method called check parentheses
+
+    the method should take in a string
+    and return a boolean
+
+    return true if the string contains "correct" pairs of parentheses
+    return false if not
+
+    situations to return false:
+    more opening or closing brackets of a particular type (), <>, {}, []
+    closing bracket at start
+    opening brackets followed by two closing brackets
+
+
+    can look at the tests to see what the different situations would be
+    */
+}
